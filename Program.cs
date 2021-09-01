dac3d0c4aab92f1eb2b57a08f7111232bc012a90
wasif farid <wsffarid1@gmail.com>
2021-09-01 19:58:07 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace line_comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison computation program ");
+        }
+    }
+}
